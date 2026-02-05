c58cd466de387ea96f4065abb18626b4b3f5a607
Yuuki <nicholas.yap28@gmail.com>
2026-02-04 23:29:12 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,12 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
+import { defineConfig } from 'astro/config'
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  site: 'https://yuuki-ny.github.io',
 });
